4061dd6b331db01860fd61d60f87042aa64d56b3
Javier Valdez <javier.valdez.15v@gmail.com>
2026-08-12 10:10:53 -0600

    Adapt DagBag tests to Airflow 3

--- tests/test_dags.py
+++ tests/test_dags.py
@@ -2,12 +2,12 @@ from airflow.models.dagbag import DagBag
 
 
 def test_all_dags_import_without_errors():
-    dag_bag = DagBag(dag_folder="dags", include_examples=False)
+    dag_bag = DagBag(dag_folder="dags")
     assert dag_bag.import_errors == {}
 
 
 def test_example_dag_has_expected_tasks():
-    dag_bag = DagBag(dag_folder="dags", include_examples=False)
+    dag_bag = DagBag(dag_folder="dags")
     dag = dag_bag.get_dag("201700703_conceptos_basicos")
     assert dag is not None
     assert len(dag.tasks) == 6
